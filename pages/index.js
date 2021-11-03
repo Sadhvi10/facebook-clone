3096630997019f89fad7be4336f6d5d2d7fa1a98
Sadhvi10 <sadhvip101@gmail.com>
2021-11-03 20:48:23 +0530

    Fixed index page

--- pages/index.js
+++ pages/index.js
@@ -10,29 +10,20 @@ import { db } from "../firebase";
 export default function Home({ session, posts }) {
   if (!session) return <Login />;
   return (
-    <>
-      <div className='h-screen overflow-hidden bg-gray-100'>
-        <Head>
-          <title>Facebook</title>
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
+    <div className='h-screen overflow-hidden bg-gray-100'>
+      <Head>
+        <title>Facebook</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-        <Header />
+      <Header />
 
-        <main className='flex'>
-          <Sidebar />
-          <Feed posts={posts} />
-          <Widgets />
-        </main>
-      </div>
-      <div class='fixed z-50 bottom-2 right-2 bg-blue-100 text-blue-700 text-center font-semibold rounded-full py-2 w-20'>
-        <span class='block md:hidden'>sm</span>
-        <span class='hidden md:block lg:hidden'>md</span>
-        <span class='hidden lg:block xl:hidden'>lg</span>
-        <span class='hidden xl:block 2xl:hidden'>xl</span>
-        <span class='hidden 2xl:block'>2xl</span>
-      </div>
-    </>
+      <main className='flex'>
+        <Sidebar />
+        <Feed posts={posts} />
+        <Widgets />
+      </main>
+    </div>
   );
 }
 
